5b58f1425835a52fd2f63fb472739fa6c8718ca8
C.C <fanweixiao@gmail.com>
2024-03-12 18:05:14 +0800

    feat: add Cloudflare azure AI Gateway support

---
+++ pkg/bridge/ai/provider/cloudflare-ai-gateway-azure/provider.go
@@ -0,0 +1,163 @@
+// Package azopenai is used to provide the Azure OpenAI service
+package cloudflareAzureGateway
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	// automatically load .env file
+	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/yomorun/yomo/ai"
+	"github.com/yomorun/yomo/core/metadata"
+	"github.com/yomorun/yomo/core/ylog"
+	"github.com/yomorun/yomo/pkg/bridge/ai/provider/azopenai"
+	"github.com/yomorun/yomo/pkg/bridge/ai/register"
+)
+
+// CloudflareAzureProvider is the provider for Azure OpenAI
+type CloudflareAzureProvider struct {
+	APIKey       string
+	Resource     string
+	DeploymentID string
+	APIVersion   string
+	CfEndpoint   string
+}
+
+// NewProvider creates a new AzureOpenAIProvider
+func NewProvider(cfEndpoint string, apiKey string, resource string, deploymentID string, apiVersion string) *CloudflareAzureProvider {
+	if cfEndpoint == "" || apiKey == "" || resource == "" || deploymentID == "" || apiVersion == "" {
+		ylog.Error("parameters are required", "cfEndpoint", cfEndpoint, "apiKey", apiKey, "resource", resource, "deploymentID", deploymentID, "apiVersion", apiVersion)
+		os.Exit(-1)
+		return nil
+	}
+	ylog.Debug("CloudflareAzureProvider", "cfEndpoint", cfEndpoint, "apiKey", apiKey, "resource", resource, "deploymentID", deploymentID, "apiVersion", apiVersion)
+	return &CloudflareAzureProvider{
+		CfEndpoint:   cfEndpoint,   // https://gateway.ai.cloudflare.com/v1/111111111111111111/ai-cc-test
+		APIKey:       apiKey,       // azure api key
+		Resource:     resource,     // azure resource
+		DeploymentID: deploymentID, // azure deployment id
+		APIVersion:   apiVersion,   // azure api version
+	}
+}
+
+// Name returns the name of the provider
+func (p *CloudflareAzureProvider) Name() string {
+	return "cloudflare_azure"
+}
+
+// GetChatCompletions get chat completions for ai service
+func (p *CloudflareAzureProvider) GetChatCompletions(userInstruction string, md metadata.M) (*ai.InvokeResponse, error) {
+	tcs, err := register.ListToolCalls(md)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(tcs) == 0 {
+		ylog.Error("tools is empty")
+		return &ai.InvokeResponse{Content: "no toolcalls"}, ai.ErrNoFunctionCall
+	}
+
+	// messages
+	messages := []azopenai.ReqMessage{
+		{Role: "system", Content: `You are a very helpful assistant. Your job is to choose the best possible action to solve the user question or task. Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous. If you don't know the answer, stop the conversation by saying "no func call".`},
+		{Role: "user", Content: userInstruction},
+	}
+
+	// prepare tools
+	toolCalls := make([]ai.ToolCall, len(tcs))
+	idx := 0
+	for _, tc := range tcs {
+		toolCalls[idx] = tc
+		idx++
+	}
+
+	body := azopenai.ReqBody{Messages: messages, Tools: toolCalls}
+
+	ylog.Debug("request", "tools", len(toolCalls), "messages", messages)
+
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	url := fmt.Sprintf("%s/azure-openai/%s/%s/chat/completions?api-version=%s", p.CfEndpoint, p.Resource, p.DeploymentID, p.APIVersion)
+	ylog.Debug("chat completions request", "url", url)
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("api-key", p.APIKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	ylog.Debug("response", "body", respBody)
+
+	// slog.Info("response body", "body", string(respBody))
+	if resp.StatusCode >= 400 {
+		// log.Println(resp.StatusCode, string(respBody))
+		// {"error":{"code":"429","message": "Requests to the ChatCompletions_Create Operation under Azure OpenAI API version 2023-12-01-preview have exceeded token rate limit of your current OpenAI S0 pricing tier. Please retry after 22 seconds. Please go here: https://aka.ms/oai/quotaincrease if you would like to further increase the default rate limit."}}
+		return nil, fmt.Errorf("ai response status code is %d", resp.StatusCode)
+	}
+
+	var respBodyStruct azopenai.RespBody
+	err = json.Unmarshal(respBody, &respBodyStruct)
+	if err != nil {
+		return nil, err
+	}
+	// fmt.Println(string(respBody))
+	// TODO: record usage
+	// usage := respBodyStruct.Usage
+	// log.Printf("Token Usage: %+v\n", usage)
+
+	choice := respBodyStruct.Choices[0]
+	ylog.Debug(">>finish_reason", "reason", choice.FinishReason)
+
+	calls := respBodyStruct.Choices[0].Message.ToolCalls
+	content := respBodyStruct.Choices[0].Message.Content
+
+	ylog.Debug("--response calls", "calls", calls)
+
+	result := &ai.InvokeResponse{}
+	if len(calls) == 0 {
+		result.Content = content
+		return result, ai.ErrNoFunctionCall
+	}
+
+	// functions may be more than one
+	for _, call := range calls {
+		for tag, tc := range tcs {
+			if tc.Equal(&call) {
+				// Use toolCalls because tool_id is required in the following llm request
+				if result.ToolCalls == nil {
+					result.ToolCalls = make(map[uint32][]*ai.ToolCall)
+				}
+
+				// push the `call` instead of `call.Function` as describes in
+				// https://cookbook.openai.com/examples/function_calling_with_an_openapi_spec
+				currentCall := call
+				result.ToolCalls[tag] = append(result.ToolCalls[tag], &currentCall)
+			}
+		}
+	}
+
+	// sfn maybe disconnected, so we need to check if there is any function call
+	if len(result.ToolCalls) == 0 {
+		return nil, ai.ErrNoFunctionCall
+	}
+	return result, nil
+}
